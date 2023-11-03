e5dd3fc95adec51adf050ca3e07761faf7fdaced
Kaustav Deka <116963260+deka27@users.noreply.github.com>
2023-11-03 15:53:16 +0530

    +update to logo

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -39,7 +39,7 @@ export const Testimonials = () => (
           Testimonials
         </div>
         <div className="custom-block-big-title text-center mb-16 px-8 sm:px-24 md:px-48">
-          People like you love Tailcast
+          People like you love BabyShark
         </div>
 
         <div className="flex flex-col lg:flex-row gap-8 lg:gap-5 xl:gap-10 px-6 xl:px-0 items-center">
